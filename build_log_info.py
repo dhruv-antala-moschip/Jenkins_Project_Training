2d393e604327bddbcc9e4faaafc735625ae3585e
dhruv-antala-moschip <dhruv.antala@moschip.com>
2025-01-21 11:44:00 +0530

    Update build_log_info.py

--- build_log_info.py
+++ build_log_info.py
@@ -1,6 +1,8 @@
 import argparse
 from jenkinsapi.jenkins import Jenkins
 import logging
+import xml.etree.ElementTree as Tree
+from xml.dom import minidom
 
 JENKINS_URL = "http://localhost:8080"
 JENKINS_USER = "dhruv_antala1"
@@ -8,7 +10,7 @@ JENKINS_PASS = "Dhruv.antala##1"
 
 logger=logging.getLogger("log_info")
 logger.setLevel(logging.INFO)
-logging.basicConfig(filename='mylog.log',filemode='w')
+logging.basicConfig(filename='logreport.log', filemode='w')
 
 consoleHandler=logging.StreamHandler()
 consoleformater=logging.Formatter('%(asctime)s- %(levelname)s-%(message)s')
@@ -58,3 +60,49 @@ try:
     get_last_build_details(jenkins,job_name)
 except ConnectionError as e:
     print(f"Error :- {e}")
+
+# Build xml tree
+root = Tree.Element("testsuites")
+log_details = []
+
+with open('logreport.log', 'r') as log_file:
+    for index, line in enumerate(log_file):
+        if index <= 10:
+            log_details.append(f"{index}: {line.strip()}")
+
+for data in range(10):
+    testsuite = Tree.Element("testsuite", {
+        "name": f"Suite-{data}",
+        "tests": "0",
+        "failures": "1",
+        "errors": "0",
+        "skipped": "0",
+        "time": "0.2"
+    })
+
+    testcase = Tree.Element("testcase", {
+        "name": log_details[data],
+        "classname": f"Suite-{data}",
+        "time": "0.123"
+    })
+
+    if data % 2 != 0:
+        failure = Tree.Element("failure", {
+            "message": f"Failed test{data}",
+            "type": "AssertionError"
+        })
+        failure.text = f"more detail about failure-{data}."
+        testcase.append(failure)
+    elif data >= 6:
+        skipped = Tree.Element("skipped", {
+            "message": f"this test {data} was skipped by user"
+        })
+        testcase.append(skipped)
+    testsuite.append(testcase)
+    root.append(testsuite)
+
+rough_string = Tree.tostring(root, 'utf-8')
+parsed = minidom.parseString(rough_string)
+pretty_xml = parsed.toprettyxml(indent="  ")
+with open('report.xml', 'w') as file:
+    file.write(pretty_xml)
